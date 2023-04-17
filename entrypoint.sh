3d0aed9035342597a5db17f8db390ea5b13e9325
hghvpn1 <123541094+hghvpn1@users.noreply.github.com>
2023-04-17 08:48:57 +0330

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 # 定义 UUID 及伪装路径、哪吒面板参数，请自行修改. (注意:伪装路径以 / 符号开始,为避免不必要的麻烦,请不要使用特殊符号.)
-UUID='de04add9-5c68-8bab-950c-08cd5320df18'
-VMESS_WSPATH='/vmess'
-VLESS_WSPATH='/vless'
-TROJAN_WSPATH='/trojan'
-SS_WSPATH='/shadowsocks'
+UUID='9e8f077a-eac9-40c1-bf56-7138df53be68'
+VMESS_WSPATH='/9e8f077a-eac9-40c1-bf56-7138df53be68-vmess'
+VLESS_WSPATH='/9e8f077a-eac9-40c1-bf56-7138df53be68-vless'
+TROJAN_WSPATH='/9e8f077a-eac9-40c1-bf56-7138df53be68-trojan'
+SS_WSPATH='/9e8f077a-eac9-40c1-bf56-7138df53be68-shadowsocks'
 NEZHA_SERVER=''
 NEZHA_PORT=''
 NEZHA_KEY=''
